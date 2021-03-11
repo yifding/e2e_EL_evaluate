7ee6e334c8d175efcd2cdaa163bcf1af277d7415
Yifan Ding <dyf0125@gmail.com>
2021-03-11 18:01:43 -0500

    house cleaning of wned

---
+++ prepare_data/wned/gen_anno_from_ori_xml.py
@@ -0,0 +1,123 @@
+import os
+from collections import defaultdict
+
+
+def gen_anno_from_xml(prefix, dataset):
+    raw_text_prefix = os.path.join(prefix, dataset + '/' + 'RawText')
+    xml_file = os.path.join(prefix, dataset + '/' + dataset + '.xml')
+    doc_names = os.listdir(raw_text_prefix)
+
+    # collect documentation for each doc_name
+    doc_name2txt = dict()
+
+    for doc_name in doc_names:
+        txt_path = os.path.join(raw_text_prefix, doc_name)
+        txt = ''
+        with open(txt_path, 'r') as reader:
+            for line in reader:
+                txt += line
+        doc_name2txt[doc_name] = txt.replace('&amp;', '&')
+
+
+    # collect mention/entity annotation from xml
+    doc_name2anno = defaultdict(list)
+    # nested named entity recognition problem in silver + gold
+    reader = open(xml_file, 'r')
+
+    doc_str_start = 'document docName=\"'
+    doc_str_end = '\">'
+
+    line = reader.readline()
+    num_el_anno = 0
+    num_ner_anno = 0
+    num_shift_anno = 0
+    num_change_length = 0
+    cur_doc_name = ''
+
+    while line:
+        if doc_str_start in line:
+            start = line.find(doc_str_start)
+            end = line.find(doc_str_end)
+            cur_doc_name = line[start + len(doc_str_start): end]
+            cur_doc_name = cur_doc_name.replace('&amp;', '&')
+            assert cur_doc_name in doc_name2txt
+
+        else:
+            if '<annotation>' in line:
+                line = reader.readline()
+                assert '<mention>' in line and '</mention>' in line
+
+                m_start = line.find('<mention>') + len('<mention>')
+                m_end = line.find('</mention>')
+
+                cur_mention = line[m_start: m_end]
+                cur_mention = cur_mention.replace('&amp;', '&')
+
+                line = reader.readline()
+                # assert '<wikiName>' in line and '</wikiName>' in line
+                e_start = line.find('<wikiName>') + len('<wikiName>')
+                e_end = line.find('</wikiName>')
+                cur_ent_title = '' if '<wikiName/>' in line else line[e_start: e_end]
+
+                line = reader.readline()
+                assert '<offset>' in line and '</offset>' in line
+                off_start = line.find('<offset>') + len('<offset>')
+                off_end = line.find('</offset>')
+                offset = int(line[off_start: off_end])
+
+                line = reader.readline()
+                assert '<length>' in line and '</length>' in line
+                len_start = line.find('<length>') + len('<length>')
+                len_end = line.find('</length>')
+                length_record = int(line[len_start: len_end])
+                length = len(cur_mention)
+
+                if length != length_record:
+                    print('mention', cur_mention, 'offset', offset, 'length', length, 'length_record', length_record)
+                    num_change_length += 1
+
+                line = reader.readline()
+                if '<entity/>' in line:
+                    line = reader.readline()
+
+                assert '</annotation>' in line
+
+                # if cur_ent_title != 'NIL' and cur_ent_title != '':
+                assert cur_doc_name != ''
+                ele = {
+                        'start': offset,
+                        'end': offset + length,
+                        'mention_txt': cur_mention,
+                        'entity_txt': cur_ent_title,
+                    }
+
+                doc_txt = doc_name2txt[cur_doc_name]
+                pos_mention = doc_txt[offset: offset + length]
+                if pos_mention != ele['mention_txt']:
+                    num_shift_anno += 1
+                    offset = max(0, offset - 10)
+                    while pos_mention != cur_mention:
+                        offset = offset + 1
+                        pos_mention = doc_txt[offset: offset + length]
+                        if offset > ele['start'] + 100:
+                            print('pos_mention', doc_txt[anno['offset']: anno['offset'] + length], anno['mention_txt'])
+                            raise ValueError('huge error!')
+                    ele['start'] = offset
+                    ele['end'] = offset + length
+
+                if cur_ent_title != 'NIL' and cur_ent_title != '':
+                    num_el_anno += 1
+                else:
+                    num_ner_anno += 1
+
+                doc_name2anno[cur_doc_name].append(ele)
+        line = reader.readline()
+
+    print(
+        'num_ner_anno', num_ner_anno,
+        'num_el_anno', num_el_anno,
+        'num_shift_anno', num_shift_anno,
+        'num_change_length', num_change_length
+    )
+
+    return doc_name2txt, doc_name2anno
---
+++ prepare_data/wned/ori_xml2revise_xml.py
@@ -0,0 +1,134 @@
+import os
+import argparse
+from urllib.parse import quote
+from collections import defaultdict
+
+import stanza
+from tqdm import tqdm
+
+from gen_anno_from_ori_xml import gen_anno_from_xml
+
+
+TOKENIZER = stanza.Pipeline(lang='en', processors='tokenize')
+
+
+class Token(object):
+    def __init__(self, text, start, end, mention='', entity=''):
+        self.text = text
+        self.start = start
+        self.end = end
+
+        self.mention = mention
+        self.entity = entity
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return "'text': " + self.text \
+            + " 'start': " + str(self.start) \
+            + " 'end': " + str(self.end) \
+            + " 'mention': " + str(self.mention) \
+            + " 'entity': " + str(self.entity)
+
+
+def write_xml(dataset, xml_file, redict):
+    print('ready to write:', 'dataset', dataset, 'path', xml_file)
+
+    with open(xml_file, 'w') as writer:
+        writer.write('<?xml version="1.0" encoding="UTF-8" standalone="no"?>' + '\n')
+        writer.write('<' + dataset + '.entityAnnotation>' + '\n')
+        for document in sorted(redict.keys()):
+            tmps = redict[document]
+            document = document.replace(' ', '_').replace('&', '&amp;')
+
+            # <document docName="20001115_AFP_ARB.0093.eng">
+            writer.write('\t' + '<document docName="' + document + '">' + '\n')
+            for anno in tmps:
+                # a preparation
+                mention_txt = anno['mention_txt']
+                entity_txt = anno['entity_txt']
+                start = anno['start']
+                end = anno['end']
+
+                mention_txt = mention_txt.replace('_', ' ').replace('&', '&amp;')
+                entity_txt = entity_txt.replace('_', ' ').replace('&', '&amp;')
+
+                # b write things down
+                writer.write('\t\t' + '<annotation>' + '\n')
+
+                writer.write('\t\t\t' + '<mention>' + mention_txt + '</mention>' + '\n')
+                writer.write('\t\t\t' + '<wikiName>' + entity_txt + '</wikiName>' + '\n')
+                writer.write('\t\t\t' + '<offset>' + str(start) + '</offset>' + '\n')
+                writer.write('\t\t\t' + '<length>' + str(end - start) + '</length>' + '\n')
+
+                writer.write('\t\t' + '</annotation>' + '\n')
+
+            writer.write('\t' + '</document>' + '\n')
+        writer.write('</' + dataset + '.entityAnnotation>' + '\n')
+
+
+def process_wned(args):
+    prefix = args.input_dir
+    data_names = args.data_names
+    output_prefix = args.output_dir
+    os.makedirs(output_prefix, exist_ok=True)
+
+    # check the annotation
+    for data_name in data_names:
+        print('start to check the dataset: ', data_name)
+        doc_name2txt, doc_name2anno = gen_anno_from_xml(prefix, data_name)
+        for doc_name in doc_name2anno:
+            assert doc_name in doc_name2txt
+            doc_txt = doc_name2txt[doc_name]
+            doc_anno = doc_name2anno[doc_name]
+            for anno in doc_anno:
+                start, end, mention_txt, entity_txt = \
+                    anno['start'], anno['end'], anno['mention_txt'], anno['entity_txt']
+                assert end - start == len(mention_txt)
+                assert doc_txt[start: end] == mention_txt
+        print('check the dataset: ', data_name, 'successfully')
+
+    for data_name in data_names:
+        print('start to process dataset:', data_name)
+        doc_name2txt, doc_name2anno = gen_anno_from_xml(prefix, data_name)
+        xml_file = os.path.join(args.output_dir, data_name + '/' + data_name + '.xml')
+        txt_dir = os.path.join(args.output_dir, data_name + '/RawText')
+        os.makedirs(txt_dir, exist_ok=True)
+
+        # **YD** sort the annotations by start first, and end second.
+        for doc_name in doc_name2anno:
+            tmp = doc_name2anno[doc_name]
+            tmp = sorted(tmp, key=lambda x: x['end'])
+            tmp = sorted(tmp, key=lambda x: x['start'])
+            doc_name2anno[doc_name] = list(tmp)
+
+            txt_file = os.path.join(txt_dir, doc_name)
+            with open(txt_file, 'w') as writer:
+                writer.write(doc_name2txt[doc_name])
+
+        write_xml(data_name, xml_file, doc_name2anno)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(allow_abbrev=False)
+    parser.add_argument(
+        "--input_dir",
+        default="/scratch365/yding4/EL_resource/data/raw/wned-datasets/",
+        type=str,
+    )
+
+    parser.add_argument(
+        "--output_dir",
+        default="/scratch365/yding4/e2e_EL_evaluate/data/processed/wned/ori_xml2revise_xml",
+        type=str,
+    )
+
+    parser.add_argument(
+        "--data_names",
+        default="['ace2004', 'aquaint', 'clueweb', 'msnbc', 'wikipedia']",
+        type=eval,
+    )
+
+    args = parser.parse_args()
+    process_wned(args)
