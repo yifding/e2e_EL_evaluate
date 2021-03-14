b42d886931190501299c44ca8692dca47a2d52dd
Yifan Ding <dyf0125@gmail.com>
2021-03-13 19:49:50 -0500

    update running file

--- baseline/deep_ed_PyTorch/run_files/step5.sh
+++ baseline/deep_ed_PyTorch/run_files/step5.sh
@@ -9,7 +9,7 @@
 export PATH=/afs/crc.nd.edu/user/y/yding4/.conda/envs/hetseq/bin:$PATH
 export LD_LIBRARY_PATH=/afs/crc.nd.edu/user/y/yding4/.conda/envs/hetseq/lib:$LD_LIBRARY_PATH
 
-CODE_DIR=/scratch365/yding4/EL_resource/baseline/deep_ed_PyTorch/deep_ed_PyTorch
-DATA_PATH=/scratch365/yding4/EL_resource/data/deep_ed_PyTorch_data
+CODE_DIR=/scratch365/yding4/e2e_EL_evaluate/baseline/deep_ed_PyTorch/deep_ed_PyTorch
+DATA_PATH=/scratch365/yding4/e2e_EL_evaluate/data/deep_ed_PyTorch_data
 
 python3 ${CODE_DIR}/data_gen/gen_test_train_data/gen_test_train_data.py --root_data_dir ${DATA_PATH}
--- baseline/deep_ed_PyTorch/run_files/step8.sh
+++ baseline/deep_ed_PyTorch/run_files/step8.sh
@@ -9,8 +9,8 @@
 export PATH=/afs/crc.nd.edu/user/y/yding4/.conda/envs/hetseq/bin:$PATH
 export LD_LIBRARY_PATH=/afs/crc.nd.edu/user/y/yding4/.conda/envs/hetseq/lib:$LD_LIBRARY_PATH
 
-CODE_DIR=/scratch365/yding4/EL_resource/baseline/deep_ed_PyTorch/deep_ed_PyTorch
-DATA_PATH=/scratch365/yding4/EL_resource/data/deep_ed_PyTorch_data
+CODE_DIR=/scratch365/yding4/e2e_EL_evaluate/baseline/deep_ed_PyTorch/deep_ed_PyTorch
+DATA_PATH=/scratch365/yding4/e2e_EL_evaluate/data/deep_ed_PyTorch_data
 
 python3 ${CODE_DIR}/entities/relatedness/filter_wiki_canonical_words_RLTD.py --root_data_dir ${DATA_PATH}
 python3 ${CODE_DIR}/entities/relatedness/filter_wiki_hyperlink_contexts_RLTD.py --root_data_dir ${DATA_PATH}
