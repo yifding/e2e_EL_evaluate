5c8e25496c39edc747a9d3866a3ba319c6f5ad53
Yifan Ding <dyf0125@gmail.com>
2021-05-08 19:05:31 -0400

    5_8 running script

---
+++ RUN_FILES/5_8_2021_analysis/collect_pkl_from_verified_anno.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#$-m abe
+#$-M yding4@nd.edu
+#$-q gpu # specify the queue
+#$-l gpu_card=4
+#$-N CRC_collect_pkl_from_verified_anno
+
+export PATH=/afs/crc.nd.edu/user/y/yding4/.conda/envs/e2e_EL_evaluate/bin:$PATH
+export LD_LIBRARY_PATH=/afs/crc.nd.edu/user/y/yding4/.conda/envs/e2e_EL_evaluate/lib:$LD_LIBRARY_PATH
+
+CODE=/scratch365/yding4/e2e_EL_evaluate/e2e_EL_evaluate/process_db_data/collect_pkl_from_verified_anno.py
+
+INPUT_DIR=/scratch365/yding4/e2e_EL_evaluate/data/5_8_2021_analysis/download_db2disk
+SOURCE_XML_DIR=/scratch365/yding4/e2e_EL_evaluate/data/prepare_split/EL
+REWRITE_XML_OUTPUT_DIR=/scratch365/yding4/e2e_EL_evaluate/data/5_8_2021_analysis/collect_pkl_rewrite_xml_EL
+LABEL_XML_OUTPUT_DIR=/scratch365/yding4/e2e_EL_evaluate/data/5_8_2021_analysis/label_xml_EL
+
+python ${CODE} \
+    --input_dir ${INPUT_DIR}    \
+    --source_xml_dir    ${SOURCE_XML_DIR}   \
+    --rewrite_xml_output_dir    ${REWRITE_XML_OUTPUT_DIR}   \
+    --label_xml_output_dir  ${LABEL_XML_OUTPUT_DIR}
