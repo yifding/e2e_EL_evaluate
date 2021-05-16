f57248a5f13b9dc5121d0e86f0e59f68f3ed7e3d
Yifan Ding <dyf0125@gmail.com>
2021-05-16 14:56:00 -0400

    verify_rate

---
+++ RUN_FILES/5_13_2021/verify_rate/wned/GT_msnbc.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#$-m abe
+#$-M yding4@nd.edu
+#$-q gpu # specify the queue
+#$-l gpu_card=4
+#$-N CRC_edit_entity_from_two_xml
+
+export PATH=/afs/crc.nd.edu/user/y/yding4/.conda/envs/e2e_EL_evaluate/bin:$PATH
+export LD_LIBRARY_PATH=/afs/crc.nd.edu/user/y/yding4/.conda/envs/e2e_EL_evaluate/lib:$LD_LIBRARY_PATH
+
+CODE=/scratch365/yding4/e2e_EL_evaluate/e2e_EL_evaluate/evaluation/obtain_verify_edit_entity_from_two_xml.py
+
+
+python ${CODE}  \
+    --subset_xml_dir    /scratch365/yding4/e2e_EL_evaluate/data/5_13_2021/label_xml_EL \
+    --full_xml_dir  /scratch365/yding4/e2e_EL_evaluate/data/5_13_2021/collect_pkl_rewrite_xml_EL   \
+    --datasets  "['msnbc']"    \
+    --models    "['GT']"   > GT_msnbc.log
+
+# --datasets  "['ace2004','aquaint','clueweb','msnbc','wikipedia','aida_testa','aida_testb','aida_train']"    \
+# --models    "['GT','rel','end2end_neural_el']"
