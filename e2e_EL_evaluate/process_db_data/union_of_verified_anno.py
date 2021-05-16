0fecd1aadf4911720fd1d76b96ab47b8f693dda5
Yifan Ding <dyf0125@gmail.com>
2021-05-16 15:54:19 -0400

    add models and datasets

--- e2e_EL_evaluate/process_db_data/union_of_verified_anno.py
+++ e2e_EL_evaluate/process_db_data/union_of_verified_anno.py
@@ -6,42 +6,55 @@ from e2e_EL_evaluate.evaluation.confusion_matrix_from_xml import confusion_matri
 
 def main(args):
     # 1. obtain the union of annotations.
-    dataset_type = DATASET2DATASET_TYPES[args.dataset]
-    union_doc_name2txt = dict()
-    union_doc_name2anno = dict()
-    for xml_model in XMLModel2DBModel:
-        input_dir = os.path.join(args.input_verify_dir, xml_model + '/' + dataset_type)
-        doc_name2txt, doc_name2anno = gen_anno_from_xml(input_dir, args.dataset)
-        for doc_name in doc_name2txt:
-            if doc_name not in union_doc_name2txt:
-                union_doc_name2txt[doc_name] = doc_name2txt[doc_name]
-            else:
-                assert union_doc_name2txt[doc_name] == doc_name2txt[doc_name]
+    stats_list = []
+    for dataset in args.datasets:
+        assert dataset in DATASET2DATASET_TYPES
+        dataset_type = DATASET2DATASET_TYPES[dataset]
+        union_doc_name2txt = dict()
+        union_doc_name2anno = dict()
+        for xml_model in XMLModel2DBModel:
+            input_dir = os.path.join(args.input_verify_dir, xml_model + '/' + dataset_type)
+            doc_name2txt, doc_name2anno = gen_anno_from_xml(input_dir, dataset)
+            for doc_name in doc_name2txt:
+                if doc_name not in union_doc_name2txt:
+                    union_doc_name2txt[doc_name] = doc_name2txt[doc_name]
+                else:
+                    assert union_doc_name2txt[doc_name] == doc_name2txt[doc_name]
 
-        for doc_name in doc_name2anno:
-            if doc_name not in union_doc_name2anno:
-                union_doc_name2anno[doc_name] = []
-            for anno in doc_name2anno[doc_name]:
-                if anno not in union_doc_name2anno[doc_name]:
-                    union_doc_name2anno[doc_name].append(anno)
+            for doc_name in doc_name2anno:
+                if doc_name not in union_doc_name2anno:
+                    union_doc_name2anno[doc_name] = []
+                for anno in doc_name2anno[doc_name]:
+                    if anno not in union_doc_name2anno[doc_name]:
+                        union_doc_name2anno[doc_name].append(anno)
 
-    # 2. obtain the model annotations.
-    for xml_model in XMLModel2DBModel:
-        print('model', xml_model)
-        model_xml_dir = os.path.join(args.model_xml_dir, xml_model + '/' + dataset_type)
-        model_doc_name2txt, model_doc_name2anno = gen_anno_from_xml(model_xml_dir, args.dataset)
+        # 2. obtain the model annotations.
+        for xml_model in args.models:
+            print('model', xml_model)
+            model_xml_dir = os.path.join(args.model_xml_dir, xml_model + '/' + dataset_type)
+            model_doc_name2txt, model_doc_name2anno = gen_anno_from_xml(model_xml_dir, dataset)
 
-        # 3. update the num_anno computation in GT.
-        stats = confusion_matrix_from_xml(
-            union_doc_name2txt,
-            model_doc_name2anno,
-            union_doc_name2anno,
-            is_strong_match=args.is_strong_match,
-            method=args.method,
-        )
+            # 3. update the num_anno computation in GT.
+            stats = confusion_matrix_from_xml(
+                union_doc_name2txt,
+                model_doc_name2anno,
+                union_doc_name2anno,
+                is_strong_match=args.is_strong_match,
+                method=args.method,
+            )
 
-        metric = compute_metric(stats)
-        print(metric)
+            stats_list.append(stats)
+
+    total_stats = dict()
+    for key in stats_list[0].keys():
+        total_stats[key] = 0
+
+    for stats in stats_list:
+        for key, value in stats.items():
+            total_stats[key] += value
+
+    metric = compute_metric(total_stats)
+    print(metric)
 
 
 if __name__ == '__main__':
@@ -65,11 +78,17 @@ if __name__ == '__main__':
     )
 
     parser.add_argument(
-        '--dataset',
-        type=str,
-        default='aida_testa',
-        choices=['ace2004', 'aquaint', 'clueweb', 'msnbc', 'wikipedia', 'aida_testa', 'aida_testb', 'aida_train'],
+        '--datasets',
+        type=eval,
+        default="['ace2004','aquaint','clueweb','msnbc','wikipedia','aida_testa','aida_testb','aida_train']",
         help='datasets for EL',
+    )
+
+    parser.add_argument(
+        '--models',
+        type=eval,
+        default="['GT','rel','end2end_neural_el']",
+        help='XMLModel models for EL',
     )
 
     parser.add_argument(
@@ -88,5 +107,4 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     assert os.path.isdir(args.input_verify_dir)
-    assert args.dataset in DATASET2DATASET_TYPES
     main(args)
