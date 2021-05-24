d26f1be279a0e9bb441ce2878bee6a9b9980cf6f
Yifan Ding <dyf0125@gmail.com>
2021-05-24 17:25:06 -0400

    prehoc evaluation

---
+++ RUN_FILES/5_20_2021/pre_hoc_evaluation/rel_pre_hoc_evaluation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#$-m abe
+#$-M yding4@nd.edu
+#$-q gpu # specify the queue
+#$-l gpu_card=4
+#$-N CRC_e2e_pre_hoc_evaluation
+
+export PATH=/afs/crc.nd.edu/user/y/yding4/.conda/envs/e2e_EL_evaluate/bin:$PATH
+export LD_LIBRARY_PATH=/afs/crc.nd.edu/user/y/yding4/.conda/envs/e2e_EL_evaluate/lib:$LD_LIBRARY_PATH
+
+CODE=/scratch365/yding4/e2e_EL_evaluate/e2e_EL_evaluate/evaluation/pre_hoc_evaluation.py
+
+MODEL_XML_DIR='/scratch365/yding4/e2e_EL_evaluate/data/5_20_2021/collect_pkl_rewrite_xml_EL'
+GT_XML_DIR='/scratch365/yding4/e2e_EL_evaluate/data/5_20_2021/collect_pkl_rewrite_xml_EL'
+MODEL_MODEL='rel'
+GT_MODEL='GT'
+
+
+python ${CODE}  \
+    --model_xml_dir     ${MODEL_XML_DIR=}   \
+    --GT_xml_dir        ${GT_XML_DIR}   \
+    --model_model       ${MODEL_MODEL}  \
+    --GT_model      ${GT_MODEL} \
+    > rel_pre_hoc_evaluation.log
