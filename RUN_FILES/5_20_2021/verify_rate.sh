2339544e9378c2b964ec6f48efe72cf8493fa692
Yifan Ding <dyf0125@gmail.com>
2021-05-24 18:14:22 -0400

    correct verification rate error

--- RUN_FILES/5_20_2021/verify_rate.sh
+++ RUN_FILES/5_20_2021/verify_rate.sh
@@ -13,7 +13,7 @@ CODE=/scratch365/yding4/e2e_EL_evaluate/e2e_EL_evaluate/evaluation/obtain_verify
 
 
 python ${CODE}  \
-    --subset_xml_dir    /scratch365/yding4/e2e_EL_evaluate/data/5_13_2021/label_xml_EL \
-    --full_xml_dir  /scratch365/yding4/e2e_EL_evaluate/data/5_13_2021/collect_pkl_rewrite_xml_EL   \
+    --subset_xml_dir    /scratch365/yding4/e2e_EL_evaluate/data/5_20_2021/label_xml_EL \
+    --full_xml_dir  /scratch365/yding4/e2e_EL_evaluate/data/5_20_2021/collect_pkl_rewrite_xml_EL   \
     --datasets  "['ace2004','aquaint','clueweb','msnbc','wikipedia','aida_testa','aida_testb','aida_train']"    \
-    --models    "['GT','rel','end2end_neural_el']"   > verify_rate.log
+    --models    "['GT','rel','end2end_neural_el']"   > new_verify_rate.log
